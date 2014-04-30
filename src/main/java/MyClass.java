6772f80919bf122a570a3e2507bcb2eb781f74ea
Mina Seddik <mina_seddik@silanis.com>
2014-04-30 13:15:12 -0400

    first commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mina
+ * Date: 30/04/14
+ * Time: 1:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyClass {
+
+    public static void main(String[] ssss){
+
+        System.out.println("yes, here we are....");
+    }
+}
